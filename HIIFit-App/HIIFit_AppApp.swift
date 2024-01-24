f5e88d32b88323a29870eb3ebeb0a7162b8d853f
Vishwas Sharma <ch0nky@protonmail.com>
2024-01-24 12:58:57 +0530

    Initial Commit

---
+++ HIIFit-App/HIIFit_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HIIFit_AppApp.swift
+//  HIIFit-App
+//
+//  Created by Vishwas Sharma on 24/01/24.
+//
+
+import SwiftUI
+
+@main
+struct HIIFit_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
